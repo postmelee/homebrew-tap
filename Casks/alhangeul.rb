f1567d507f69e8b58164a879f008e754eda4b35e
postmelee <meleeisdeveloping@gmail.com>
2026-06-25 07:08:53 +0900

    Update alhangeul cask to 0.1.7

--- Casks/alhangeul.rb
+++ Casks/alhangeul.rb
@@ -1,6 +1,6 @@
 cask "alhangeul" do
-  version "0.1.6"
-  sha256 "87e37549a569813a3e22606f497ce837b350243cf3fed2ccd286af3ab8b02b9a"
+  version "0.1.7"
+  sha256 "332208ff6f68c78a49d0fc60b895eeabb41d4996dad38fde158fa1935ab4b09d"
 
   url "https://github.com/postmelee/alhangeul-macos/releases/download/v#{version}/alhangeul-macos-#{version}.dmg"
   name "알한글"
